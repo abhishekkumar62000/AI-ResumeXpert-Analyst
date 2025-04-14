1bd72a29e3a0f174f990cc509a4689c7c8a9dc99
Abhishek Yadav <159156426+abhishekkumar62000@users.noreply.github.com>
2025-04-14 17:57:04 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 # Import Important Libraries
 import streamlit as st 
-import google.generativeai as genai 
+import google.generativeai as genai
 import webbrowser
 from PyPDF2 import PdfReader
 from docx import Document
